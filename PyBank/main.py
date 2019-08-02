25f4f2b62ef1cdfa66aa005beb432b6402c16af5
Fred <efrediii@hotmail.com>
2019-08-02 06:47:36 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,6 +9,7 @@ import os
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
+output_path = os.path.join(".", "output", "Python_homework_1.5.csv")
 
 total_number_of_months = 0
 net_total = 0
@@ -42,8 +43,22 @@ with open(csvpath, newline='') as csvfile:
         last_profit_loss = int(row[1])
 avg_change = avg_change / total_number_of_months
 
+open(output_path, 'w', newline='') as csvfile:
+csvwriter = csv.writer(csvfile, delimiter=',')
+#csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+
+
 print("Total Months: " + str(total_number_of_months))
+csvwriter.writerow(['"Total Months: " + str(total_number_of_months)'])
+
 print("Total: $" + str(net_total))
+csvwriter.writerow(['"Total Months: " + str(total_number_of_months)'])
+
 print("Average Change: $" + str(avg_change))
+csvwriter.writerow(['"Average Change: $" + str(avg_change)'])
+
 print("Greatest Increase in Profits: " + str(greatest_increase_date) + " ($" + str(greatest_increase) + ")" )
+csvwriter.writerow(['"Greatest Increase in Profits: " + str(greatest_increase_date) + " ($" + str(greatest_increase) + ")"'])
+
 print("Greatest Decrease in Profits: " + str(greatest_decrease_date) + " ($" + str(greatest_decrease) + ")" )
+csvwriter.writerow(['("Greatest Decrease in Profits: " + str(greatest_decrease_date) + " ($" + str(greatest_decrease) + ")"'])
