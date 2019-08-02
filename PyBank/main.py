7cb9fd3da6d4702b1b9b982aec27865a05f49035
Fred <efrediii@hotmail.com>
2019-08-02 05:02:08 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,9 @@ csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 total_number_of_months = 0
 net_total = 0
 avg_change = 0
-
+greatest_increase = 0
+greatest_decrease = 0
+change = 0
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -26,13 +28,21 @@ with open(csvpath, newline='') as csvfile:
         total_number_of_months += 1
         net_total += int(row[1])
 
-        last_profit_loss = int(row[1])
         if total_number_of_months > 1:
-            avg_change += int(row[1]) - last_profit_loss
+            change = int(row[1]) - last_profit_loss
+            avg_change += change
+            if change > greatest_increase:
+                greatest_increase = change
+            if change < greatest_decrease:
+                greatest_decrease = change
 
-        avg_change = avg_change / total_number_of_months
+        last_profit_loss = int(row[1])
+        
+avg_change = avg_change / total_number_of_months
             
 
 print("Total months = " + str(total_number_of_months))
 print("net_total = " + str(net_total))
 print("avg_change = " + str(avg_change))
+print("greatest_increase = " + str(greatest_increase))
+print("greatest_decrease = " + str(greatest_decrease))
