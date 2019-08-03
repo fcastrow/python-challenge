a55465e6cd8e9818a545402f6428561ae8280b4d
Fred <efrediii@hotmail.com>
2019-08-03 15:41:45 -0500

    Hey there!

--- PyPoll/main.py.first_attempt.py
+++ PyPoll/main.py.first_attempt.py
@@ -4,16 +4,14 @@
 #
 #This is my second Python Program
 #
-#2019.08.03
-#simplify, quit trying to use a list of two lists, just use two lists...
 
 import os
 import csv
+
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 output_path = os.path.join(".", "output", "Python_homework_2.5.txt")
 
-candidate_list = []
-candidate_votes_list = []
+candidate_vote_lists_in_a_list = [[],[]]
 
 total_number_of_votes = 0
 candidate_count = 0
@@ -26,17 +24,17 @@ with open(csvpath, newline='') as csvfile:
         total_number_of_votes += 1
         candidate = row[2]
         candidate_found = False
-
-        for candidate_index in candidate_list:
-            if( candidate == candidate_list[candidate_index] ):
-                candidate_votes_list[candidate_index] += 1
+#        for candidate_index == 0 to candidate_count:
+        for candidate_index in candidate_vote_lists_in_a_list[0]:
+            if( candidate == candidate_vote_lists_in_a_list[candidate_index] ):
+                candidate_vote_lists_in_a_list[1] += 1
                 candidate_found = True
                 break
 
         if not(candidate_found):
             candidate_count += 1
-            candidate_list[candidate_count] = candidate
-            candidate_votes_list[candidate_count] = 1
+            candidate_vote_lists_in_a_list[candidate_count] = candidate
+            candidate_vote_lists_in_a_list[candidate_count, 0] = 1
             
             
 
